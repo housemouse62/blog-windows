16c3c7fd16a882e83cdf21d52d483b986f103fde
housemouse62 <ryan.skeels@gmail.com>
2026-05-04 22:33:24 +0200

    build comment post, update, delete routes

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -7,29 +7,114 @@ const commentRouter = Router();
 
 commentRouter.get("/:postID/comments", async (req, res, next) => {
   try {
-    const comments = await prisma.comment.findMany({
-      where: { postID: parseInt(req.params.postID) },
-    });
+    const postID = parseInt(req.params.postID);
+
     if (isNaN(postID)) {
       const err = new Error("Post not found");
       err.status = 400;
       return next(err);
     }
+
+    const comments = await prisma.comment.findMany({
+      where: { postID: postID },
+    });
+
     res.status(200).json(comments);
   } catch (err) {
     next(err);
   }
 });
 
-commentRouter.get("/:postID/comments/:commentID", async (req, res, next) => {
+commentRouter.post("/:postID/comments", verifyToken, async (req, res, next) => {
   try {
+    const newComment = await prisma.comment.create({
+      data: {
+        commentbody: req.body.commentbody,
+        authorID: req.user.id,
+        postID: parseInt(req.params.postID),
+      },
+    });
+    res.status(201).json(newComment);
   } catch (err) {
     next(err);
   }
 });
-// get /posts/:postID/comments/:commentID
-// post /posts/:postID/comments
-// update /posts/:postID/comments/:commentID
-// delete /posts/:postID/comments/:commentID
+
+commentRouter.patch(
+  "/:postID/comments/:commentID",
+  verifyToken,
+  async (req, res, next) => {
+    try {
+      const commentID = parseInt(req.params.commentID);
+
+      if (isNaN(commentID))
+        return res.status(404).json({ error: "Invalid Comment ID" });
+
+      const findComment = await prisma.comment.findUnique({
+        where: { id: commentID },
+      });
+      if (!findComment) {
+        return res.status(404).json({ error: "Comment not found" });
+      }
+
+      if (req.user.id === findComment.authorID) {
+        const comment = await prisma.comment.update({
+          where: { id: commentID },
+          data: {
+            commentbody: req.body.commentbody,
+          },
+        });
+        return res.status(200).json(comment);
+      } else {
+        return res.status(403).json({ error: "Unauthorized Credentials" });
+      }
+    } catch (err) {
+      next(err);
+    }
+  },
+);
+
+commentRouter.delete(
+  "/:postID/comments/:commentID",
+  verifyToken,
+  async (req, res, next) => {
+    try {
+      const commentID = parseInt(req.params.commentID);
+      if (isNaN(commentID))
+        return res.status(404).json({ error: "Invalid Comment ID" });
+
+      const postID = parseInt(req.params.postID);
+      if (isNaN(postID))
+        return res.status(404).json({ error: "Invalid Post ID" });
+
+      const findComment = await prisma.comment.findUnique({
+        where: { id: commentID },
+      });
+      if (!findComment) {
+        return res.status(404).json({ error: "Comment Not Found" });
+      }
+
+      const findPost = await prisma.post.findUnique({
+        where: { id: postID },
+      });
+      if (!findPost) {
+        return res.status(404).json({ error: "Post Not Found" });
+      }
+      if (
+        req.user.id === findComment.authorID ||
+        req.user.id === findPost.authorID
+      ) {
+        const comment = await prisma.comment.delete({
+          where: { id: commentID },
+        });
+        return res.status(200).json(comment);
+      } else {
+        return res.status(403).json({ error: "Unauthorized Credentials" });
+      }
+    } catch (err) {
+      next(err);
+    }
+  },
+);
 
 export default commentRouter;
