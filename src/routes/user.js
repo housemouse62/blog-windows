2d7c85a5412f9bbd07f27b162836c75360915beb
housemouse62 <ryan.skeels@gmail.com>
2026-05-12 12:20:11 +0200

    add route to update user information

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,21 +5,58 @@ import { body } from "express-validator";
 import { validationResult } from "express-validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import verifyToken from "../../middleware/verifyToken.js";
 
 const authLimiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 10 });
 
 const userRouter = Router();
 
-// userRouter.get("/:userID", authLimiter (req, res, next) => {
-//   try {
-//     const user = await prisma.user.findUnique({
-//       where: { id:  },
-//     });
-//     res.json(user);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+userRouter.patch(
+  "/profile",
+  authLimiter,
+  verifyToken,
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password)
+      throw new Error("New passwords do not match.");
+    return true;
+  }),
+  async (req, res, next) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: req.user.id },
+      });
+      const updateData = {};
+      if (req.body.password) {
+        const match = await bcrypt.compare(
+          req.body.currentPassword,
+          user.password,
+        );
+        if (!match) {
+          return res.status(404).json({ error: "Invalid Credentials" });
+        }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        updateData.password = hashedPassword;
+      }
+      if (req.body.name) updateData.name = req.body.name;
+      if (req.body.email) updateData.email = req.body.email;
+      if (req.body.screenname) updateData.screenname = req.body.screenname;
+
+      const newUser = await prisma.user.update({
+        where: { id: user.id },
+        data: updateData,
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          usertype: true,
+        },
+      });
+      return res.status(200).json(newUser);
+    } catch (err) {
+      next(err);
+    }
+  },
+);
 
 userRouter.post(
   "/create",
@@ -81,6 +118,7 @@ userRouter.post("/login", async (req, res, next) => {
       email: user.email,
       name: user.name,
       usertype: user.usertype,
+      screenname: user.screenname,
     };
     jwt.sign(
       { tokenUser },
